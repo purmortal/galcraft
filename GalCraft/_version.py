376275694db2651d2b00155abe226fdfd8003db5
王梓先 <53634598+purmortal@users.noreply.github.com>
2024-10-15 13:36:13 -0600

    Delete GalCraft directory

--- GalCraft/_version.py
+++
@@ -1,7 +0,0 @@
-"""Version information."""
-# Version digit interpretation
-# First digit: major updates
-# Second digit: minor features / modifications
-# Third digit: bug fixing
-
-__version__ = "1.2.1"
--- GalCraft/modules/constant.py
+++
@@ -1,2 +0,0 @@
-# Define some constants
-c = 299792.458 # speed of light in km/s

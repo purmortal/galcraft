d47e0752305ecc71ba427477d20b6ea927af9ce4
王梓先 <wang.zixian.astro@gmail.com>
2024-10-15 23:40:40 -0600

    Update check_outputs.py

--- .github/workflows/tests/check_outputs.py
+++ .github/workflows/tests/check_outputs.py
@@ -6,7 +6,8 @@ from pathlib import Path
 # Define the directories for the test files
 test_dir = Path(__file__).parent
 true_dir = test_dir / "outputs"
-outputs_dir = "/home/runner/work/galcraft/galcraft/galcraft-test_kit/tests/outputs"
+# outputs_dir = "/home/runner/work/galcraft/galcraft/galcraft-test_kit/tests/outputs"
+outputs_dir = test_dir / "galcraft-test_kit/tests/output/test_kit"
 
 
 files_npy = ["mass_fraction_array_0.npy", "mass_fraction_array_1.npy",

383f580b235f5409308e77e5c648f60cbe11cddd
王梓先 <wang.zixian.astro@gmail.com>
2024-10-15 23:03:29 -0600

    Update check_outputs.py

--- .github/workflows/tests/check_outputs.py
+++ .github/workflows/tests/check_outputs.py
@@ -5,6 +5,6 @@ from pathlib import Path
 # Define the directories for the test files
 test_dir = Path(__file__).parent
 true_dir = test_dir / "outputs"
-outputs_dir = "/home/runner/work/galcraft/galcraft/galcraft-test_kit/tests/outputs"
-
+outputs_dir = test_dir / "galcraft-test_kit/tests/outputs"
+# "/home/runner/work/galcraft/galcraft/galcraft-test_kit/tests/outputs"
 print(outputs_dir, true_dir)
